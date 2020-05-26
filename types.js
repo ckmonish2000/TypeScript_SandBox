bf592aa3b8dc6468652797a22cf4910023138d90
ckmonish2000 <ckmonish2000@gmail.com>
2020-05-26 16:54:10 +0530

    data types

--- types.js
+++ types.js
@@ -1,9 +1,28 @@
-// let word: string;
-// word="String";
-// console.log(word);
+var word;
+word = "String";
+console.log(word);
 var num;
 num = 2000 + 5 * 2;
 console.log(num);
 var bool;
 bool = true;
 console.log(bool);
+var vars;
+vars = false;
+console.log(vars);
+// array
+var arr;
+arr = [1, 2, 3, 4];
+console.log(arr);
+var strarr;
+strarr = ["121", "cadsc"];
+console.log(strarr);
+// tuple
+var mytup;
+mytup = ["sup", 12, true];
+console.log(mytup);
+// null void and undefined
+var vid;
+vid = null;
+var mynull = null;
+var myundef = undefined;
--- types.ts
+++ types.ts
@@ -1,6 +1,6 @@
-// let word: string;
-// word="String";
-// console.log(word);
+let word: string;
+word="String";
+console.log(word);
 
 let num: number;
 num=2000+5*2;
@@ -9,3 +9,30 @@ console.log(num);
 let bool: boolean;
 bool=true;
 console.log(bool);
+
+let vars: any;
+vars=false;
+console.log(vars);
+
+// array
+let arr:number[];
+arr=[1,2,3,4];
+console.log(arr);
+
+let strarr: Array<string>;
+strarr=["121","cadsc"];
+console.log(strarr);
+
+
+// tuple
+
+let mytup: [string, number,boolean]
+
+mytup=["sup",12,true]
+console.log(mytup)
+
+// null void and undefined
+let vid:void;
+vid=null;
+let mynull:null=null;
+let myundef:undefined=undefined;
