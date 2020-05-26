968cb267aed9f1c8f3e457027ee98fda6af499ee
ckmonish2000 <ckmonish2000@gmail.com>
2020-05-26 16:36:13 +0530

    data types

---
+++ types.js
@@ -0,0 +1,9 @@
+// let word: string;
+// word="String";
+// console.log(word);
+var num;
+num = 2000 + 5 * 2;
+console.log(num);
+var bool;
+bool = true;
+console.log(bool);
---
+++ types.ts
@@ -0,0 +1,11 @@
+// let word: string;
+// word="String";
+// console.log(word);
+
+let num: number;
+num=2000+5*2;
+console.log(num);
+
+let bool: boolean;
+bool=true;
+console.log(bool);
