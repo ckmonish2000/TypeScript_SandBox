2c97c7d5ba52761e40b245fca84307063694d0fd
ckmonish2000 <ckmonish2000@gmail.com>
2020-05-26 17:00:06 +0530

    functions

---
+++ func.js
@@ -0,0 +1,7 @@
+function getnum(num1, num2) {
+    return num1 + num2;
+}
+var mysum = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(mysum(10, 3));
---
+++ func.ts
@@ -0,0 +1,9 @@
+function getnum(num1:number,num2:number):number{
+
+    return num1+num2;
+}
+let mysum=function(num1:number,num2:number):number{
+
+    return num1+num2;
+}
+console.log(mysum(10,3))
