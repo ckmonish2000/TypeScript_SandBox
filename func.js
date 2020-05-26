8b36ea3dfd60d2427211f65ce3c93f2c290de4a1
ckmonish2000 <ckmonish2000@gmail.com>
2020-05-26 17:07:51 +0530

    functions

--- func.js
+++ func.js
@@ -2,6 +2,20 @@ function getnum(num1, num2) {
     return num1 + num2;
 }
 var mysum = function (num1, num2) {
+    if (typeof num1 != 'number') {
+        num1 = parseInt(num1);
+    }
+    else if (typeof num2 != 'number') {
+        num2 = parseInt(num2);
+    }
     return num1 + num2;
 };
-console.log(mysum(10, 3));
+console.log(mysum('10', 3));
+// optional arguments
+var getname = function (first, second) {
+    if (second == undefined) {
+        return first;
+    }
+    return first + " " + second;
+};
+console.log(getname("m", "k"));
--- func.ts
+++ func.ts
@@ -2,8 +2,25 @@ function getnum(num1:number,num2:number):number{
 
     return num1+num2;
 }
-let mysum=function(num1:number,num2:number):number{
-
+let mysum=function(num1:any,num2:any):number{
+    if (typeof num1 != 'number'){
+        num1=parseInt(num1);
+    }
+    else if(typeof num2 != 'number'){
+        num2=parseInt(num2);
+    }
     return num1+num2;
 }
-console.log(mysum(10,3))
+console.log(mysum('10',3))
+
+
+// optional arguments
+
+let getname=function(first:string,second?:string):string{
+if (second==undefined){
+    return first
+}
+    return first+" "+second
+}
+
+console.log(getname("m","k"))
